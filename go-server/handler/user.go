17c41aacd8b7fca09ef1888bc5ec5354a1cda3e0
LanceHE6 <2765543491@qq.com>
2024-03-03 20:39:32 +0800

    feat.新增登录实现及修复注册bug

--- go-server/handler/user.go
+++ go-server/handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"Go_simpleWMS/utils"
 	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -14,6 +15,7 @@ func Register(context *gin.Context) {
 	account := context.PostForm("account")
 	password := context.PostForm("password")
 	permission := context.PostForm("permission")
+	nickName := context.PostForm("nick_name")
 
 	db := utils.GetDbConnection()
 
@@ -30,10 +32,24 @@ func Register(context *gin.Context) {
 		}
 	}(tx) // 如果出错，回滚事务
 
+	// 判断该账户是否已被注册
+	var registered int
+	err = tx.QueryRow("SELECT count(account) FROM user WHERE account=?", account).Scan(&registered)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot get the number of users for this account"})
+		return
+	}
+	if registered >= 1 {
+		context.JSON(http.StatusForbidden, gin.H{"message": "The account has been registered"})
+		return
+	}
 	// 获取最近注册的用户的 uid
 	var lastUid string
 	err = tx.QueryRow("SELECT uid FROM user ORDER BY register_time DESC LIMIT 1").Scan(&lastUid)
-	if err != nil {
+	// 如果没有用户，就从 1 开始
+	if errors.Is(err, sql.ErrNoRows) {
+		lastUid = "00000000"
+	} else if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot get last uid"})
 		return
 	}
@@ -51,8 +67,8 @@ func Register(context *gin.Context) {
 	registerTime := time.Now().Unix()
 
 	// 插入新用户
-	_, err = tx.Exec("INSERT INTO user (uid, account, password, permission, register_time) VALUES (?, ?, ?, ?, ?)",
-		newUid, account, password, permission, registerTime)
+	_, err = tx.Exec("INSERT INTO user (uid, account, password, nick_name, permission, register_time) VALUES (?, ?, ?, ?, ?, ?)",
+		newUid, account, password, nickName, permission, registerTime)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot insert new user"})
 		return
@@ -69,4 +85,54 @@ func Register(context *gin.Context) {
 		"message": "User registered successfully",
 		"uid":     newUid,
 	})
+}
+
+func Login(context *gin.Context) {
+	account := context.PostForm("account")
+	password := context.PostForm("password")
+
+	db := utils.GetDbConnection()
+	// 开始一个新的事务
+	tx, err := db.Begin()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot begin transaction"})
+		return
+	}
+	defer func(tx *sql.Tx) {
+		err := tx.Rollback()
+		if err != nil {
+
+		}
+	}(tx) // 如果出错，回滚事务
+
+	var uid string
+	err = tx.QueryRow("SELECT uid FROM user WHERE account = ? AND password = ?", account, password).Scan(&uid)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid account or password"})
+		return
+	} else {
+		token, err := utils.GenerateToken(uid)
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot generate token"})
+			return
+		}
+
+		// token写入数据库
+		_, err = tx.Exec("UPDATE user set token=? WHERE uid=?", token, uid)
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot update token"})
+			return
+		}
+
+		// 提交事务
+		err = tx.Commit()
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot commit transaction"})
+			return
+		}
+		context.JSON(http.StatusOK, gin.H{
+			"message": "Login successfully",
+			"token":   token})
+	}
+
 }
