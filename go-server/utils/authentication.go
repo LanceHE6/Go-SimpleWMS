64f2bc9183e4cbb3d0ecdc4af2b993eb3a550577
LanceHE6 <2765543491@qq.com>
2024-03-11 19:47:23 +0800

    feat.增加超级管理员鉴权中间件

--- go-server/utils/authentication.go
+++ go-server/utils/authentication.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 	"time"
@@ -34,14 +37,14 @@ func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "No Authorization header provided"})
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "No Authorization header provided"})
 			c.Abort()
 			return
 		}
 
 		bearerToken := strings.Split(authHeader, " ")
 		if len(bearerToken) != 2 {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Authorization header format"})
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid Authorization header format"})
 			c.Abort()
 			return
 		}
@@ -69,8 +72,50 @@ func AuthMiddleware() gin.HandlerFunc {
 			c.Set("userID", claims.Subject)
 			c.Next()
 		} else {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid token"})
 			c.Abort()
+			return
+		}
+
+	}
+}
+
+func IsSuperAdminMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// 根据token判断permission是否为3
+		token := context.GetHeader("Authorization")
+		bearerToken := strings.Split(token, " ")[1]
+		fmt.Println(bearerToken)
+		db := GetDbConnection()
+
+		// 开始一个新的事务
+		tx, err := db.Begin()
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot begin authorization transaction"})
+			context.Abort()
+			return
+		}
+		defer func(tx *sql.Tx) {
+			err := tx.Rollback()
+			if err != nil {
+
+			}
+		}(tx) // 如果出错，回滚事务
+
+		var permission string
+		err = tx.QueryRow("SELECT permission FROM user WHERE token=?", bearerToken).Scan(&permission)
+
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot get target user permission"})
+			context.Abort()
+			return
+		}
+		if permission == "3" {
+			context.Next()
+
+		} else {
+			context.JSON(http.StatusForbidden, gin.H{"message": "Permission denied"})
+			context.Abort()
 			return
 		}
 	}
