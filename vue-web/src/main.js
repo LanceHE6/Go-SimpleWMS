184fe3943c74414f6c87daf99e17b3fb4450e62a
unknown <757902252@qq.com>
2024-03-22 21:39:50 +0800

    [new]登录界面创建

--- vue-web/src/main.js
+++ vue-web/src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import { router } from './router/index'
 import App from './App.vue'
+import ElementUI from 'element-plus'
+import axios from "axios"
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = "http://192.168.8.101:8080"
+
+const app = createApp(App)
+
+app.provide("axios", axios)
+
+app.use(ElementUI)
+    .use(router)
+
+app.mount('#app')
