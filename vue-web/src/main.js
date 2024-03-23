e8c93606e56a680ac1c7e83263f4b49f8df20523
CM-Silence <757902252@qq.com>
2024-03-23 12:18:08 +0800

    [new]添加图标组件 [fix]修复组件无样式的问题

--- vue-web/src/main.js
+++ vue-web/src/main.js
@@ -3,12 +3,19 @@ import { router } from './router/index'
 import App from './App.vue'
 import ElementUI from 'element-plus'
 import axios from "axios"
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+import "element-plus/theme-chalk/el-message.css";
 
 axios.defaults.baseURL = "http://192.168.8.101:8080"
 
 const app = createApp(App)
 
 app.provide("axios", axios)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.use(ElementUI)
     .use(router)
