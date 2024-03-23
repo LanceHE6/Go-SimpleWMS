51dc4d716f0bfc50e73efe1c8a7d9d720de6ec1a
LanceHE6 <2765543491@qq.com>
2024-03-23 16:15:36 +0800

    feat.新增文字logo输出

--- go-server/main.go
+++ go-server/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"Go_simpleWMS/handler"
+	"Go_simpleWMS/handler/test"
+	"Go_simpleWMS/handler/user"
+	"Go_simpleWMS/handler/warehouse"
 	"Go_simpleWMS/utils"
+	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	fmt.Println(`
+ ________  ___  _____ ______   ________  ___       _______   ___       __   _____ ______   ________      
+|\   ____\|\  \|\   _ \  _   \|\   __  \|\  \     |\  ___ \ |\  \     |\  \|\   _ \  _   \|\   ____\     
+\ \  \___|\ \  \ \  \\\__\ \  \ \  \|\  \ \  \    \ \   __/|\ \  \    \ \  \ \  \\\__\ \  \ \  \___|_    
+ \ \_____  \ \  \ \  \\|__| \  \ \   ____\ \  \    \ \  \_|/_\ \  \  __\ \  \ \  \\|__| \  \ \_____  \   
+  \|____|\  \ \  \ \  \    \ \  \ \  \___|\ \  \____\ \  \_|\ \ \  \|\__\_\  \ \  \    \ \  \|____|\  \  
+    ____\_\  \ \__\ \__\    \ \__\ \__\    \ \_______\ \_______\ \____________\ \__\    \ \__\____\_\  \ 
+   |\_________\|__|\|__|     \|__|\|__|     \|_______|\|_______|\|____________|\|__|     \|__|\_________\
+   \|_________|                                                                              \|_________|
+	`)
 	utils.InitDB()
 	defer utils.CloseDB()
 	ginServer := gin.Default()
@@ -15,33 +28,41 @@ func main() {
 	ginServer.Use(cors.Default())
 
 	ginServer.GET("/ping", func(context *gin.Context) {
-		handler.Test(context)
+		test.Ping(context)
 	})
 
 	// 路由分组
 	userGroup := ginServer.Group("/user")
 
 	userGroup.POST("/register", func(context *gin.Context) {
-		handler.Register(context)
+		user.Register(context)
 	})
 	userGroup.POST("/login", func(context *gin.Context) {
-		handler.Login(context)
+		user.Login(context)
 	})
 	userGroup.DELETE("/delete", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		handler.DeleteUser(context)
+		user.DeleteUser(context)
 	})
 	userGroup.PUT("/update", utils.AuthMiddleware(), func(context *gin.Context) {
-		handler.UpdateUser(context)
+		user.UpdateUser(context)
 	})
 
 	warehouseGroup := ginServer.Group("/warehouse")
 
 	warehouseGroup.POST("/add", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		handler.AddWarehouse(context)
+		warehouse.AddWarehouse(context)
 	})
 
 	warehouseGroup.DELETE("/delete", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		handler.DeleteWarehouse(context)
+		warehouse.DeleteWarehouse(context)
+	})
+
+	warehouseGroup.PUT("/update", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
+		warehouse.UpdateWarehouse(context)
+	})
+
+	warehouseGroup.GET("/list", utils.AuthMiddleware(), func(context *gin.Context) {
+		warehouse.ListWarehouse(context)
 	})
 
 	err := ginServer.Run(":8080")
