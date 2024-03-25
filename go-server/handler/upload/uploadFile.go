847677ca8d12db1d0051d2fa3637c23ce7b465d1
LanceHE6 <2765543491@qq.com>
2024-03-25 23:22:47 +0800

    feat.新增文件上传接口

---
+++ go-server/handler/upload/uploadFile.go
@@ -0,0 +1,49 @@
+package upload
+
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func UploadFile(context *gin.Context) {
+	file, header, err := context.Request.FormFile("file")
+	if err == nil {
+		filename := header.Filename
+		out, err := os.Create("static/res/uploadFile/" + filename)
+		if err == nil {
+			defer func(out *os.File) {
+				err := out.Close()
+				if err != nil {
+
+				}
+			}(out)
+			_, err = io.Copy(out, file)
+			if err == nil {
+				log.Println("上传文件成功")
+				res := map[string]interface{}{
+					"filePath": "/res/uploadFile/" + filename,
+					"fileName": filename,
+				}
+				context.JSON(http.StatusOK, gin.H{
+					"message": "Upload successfully",
+					"result":  res,
+				})
+			} else {
+				context.JSON(http.StatusInternalServerError, gin.H{
+					"message": "Upload failed",
+				})
+			}
+		} else {
+			context.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Upload failed",
+			})
+		}
+	} else {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Upload failed",
+		})
+	}
+}
