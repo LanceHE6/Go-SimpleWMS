e3e809ff272388b7871a82f8d709eed90f4d6b12
TangChao132 <971926434@qq.com>
2024-03-26 12:36:42 +0800

    setting增加删除用户，新增用户请求。修改用户请求(未测试)

--- vue-web/src/views/main-views/Setting.vue
+++ vue-web/src/views/main-views/Setting.vue
@@ -16,17 +16,44 @@ import axios from "axios";
  *接收用户对象的列表
  * */
 let userArray;
-let array;
 /**
+ * 用于用户注册的对象
+ * */
+let user={
+    account:'',
+    password:'',
+    permission:'',
+    nick_name:'',
+    phone:''
+}
+
+/**
+ * 修改用户时所用到的对象
+ * */
+let userNew={
+    account:'',
+    password:'',
+    permission:'',
+    nick_name:'',
+    phone:''
+}
+
+
+const token="bearer "+localStorage.getItem("token");
+
+/**
+ * userList()
  * 获取用户的信息的请求
  * 打开网页时自动调用一次
+ * 结果：userArray中包含一到多个user对象
  * */
 
 const userList=async () => {
-    console.log("bearer "+localStorage.getItem("token"))
-    array = await axios.get('/user/list', {
+    console.log(token)
+    let array = await axios.get('/user/list', {
         headers: {
-            'Authorization': "bearer " + localStorage.getItem("token")
+            'Authorization': token
+
         }
     }).catch( error => {
         ElMessage.error("网络请求出错了！")
@@ -34,10 +61,93 @@ const userList=async () => {
     })
 
     if (array && array.data) {
-        userArray = array.data.row;
+        userArray = array.data.rows;
     }
+    console.log(userArray)
 }
 userList()
+/**
+ * userDelete()
+ * 删除用户的请求，param: uid 类型:String
+ * */
+const userDelete=async (uid) => {
+     let result = await axios({
+         method: 'delete',
+         url: '/user/delete',
+         headers: {
+             'Content-Type': 'multipart/form-data',
+             'Authorization': token
+         },
+         data: {
+             "uid":uid
+         }
+     }).catch( error => {
+        ElMessage.error("网络请求出错了！")
+        console.log(error.message)
+    })
+    console.log(result)
+}
+
+/**
+ * userRegister()
+ * 新增用户 param: userNew对象
+ * userNew={
+ *     account:'',
+ *     password:'',
+ *     permission:'',
+ *     nick_name:'',
+ *     phone:''
+ * }
+ * */
+const userRegister=async (user) => {
+    let result = await axios({
+        method: 'post',
+        url: '/user/register',
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': token
+        },
+        data: user
+    }).then(()=>{
+            userNew.account=''
+            userNew.nick_name=''
+            userNew.password=''
+            userNew.phone=''
+            userNew.permission=''
+        }
+    ).catch( error => {
+        ElMessage.error("网络请求出错了！")
+        console.log(error.message)
+    })
+    console.log(result)
+}
+
+/**
+ * userUpdate()
+ * 修改用户  param: userNew对象
+ * userNew={
+ *     account:'',
+ *     password:'',
+ *     permission:'',
+ *     nick_name:'',
+ *     phone:''
+ * */
+const userUpdate=async (userNew) => {
+    let result = await axios({
+        method: 'put',
+        url: '/user/update',
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': token
+        },
+        data: userNew
+    }).catch( error => {
+        ElMessage.error("网络请求出错了！")
+        console.log(error.message)
+    })
+    console.log(result)
+}
+userUpdate()
 </script>
 
 <style scoped>
