82023a7b6dabd94da7c70762153ddd242aebfa42
CM-Silence <757902252@qq.com>
2024-03-29 22:30:22 +0800

    [refactor]将tab做成了子组件，删去了大量的冗余代码

--- vue-web/src/views/main-views/EntryAndOut.vue
+++ vue-web/src/views/main-views/EntryAndOut.vue
@@ -5,42 +5,30 @@
         class="side-bar"
         width="collapse"
     >
-      <SideBar
+      <side-bar
           class="side-menu"
           :menu-list="sideMenu"
           @selectMenu="handleSelect"
       >
-      </SideBar>
+      </side-bar>
 
     </el-aside>
 
-
     <el-main>
-      <el-tabs
-          v-model="activeTab"
-
-          type="card"
-          class="demo-tabs"
-          @tab-remove="removeTab"
-          :closable="tabList.length > 1"
+      <my-tab
+          ref="myTab"
+          :default-tab="defaultTab"
       >
-        <el-tab-pane
-            v-for="item in tabList"
-            :label="item.label"
-            :name="item.path"
-        >
-          <router-view></router-view>
-        </el-tab-pane>
-      </el-tabs>
+      </my-tab>
     </el-main>
   </el-container>
 </template>
 
 <script setup>
 
-import {onMounted, ref, watch} from "vue";
-import {router} from "@/router/index.js";
+import {onMounted, ref} from "vue";
 import SideBar from "@/components/SideBar.vue";
+import MyTab from "@/components/MyTab.vue";
 
 onMounted(init)
 
@@ -52,62 +40,20 @@ const sideMenu = [
   {name: "调拨单", index: "/home/entryAndOut/allocate", icon: "Folder"},
 ];
 
-const activeTab = ref('/home/entryAndOut/entry')
-const tabList = ref([
-  {
+const defaultTab = {
     label: '入库单',
     path: '/home/entryAndOut/entry',
   }
-])
 
-function init(){
-  router.push(activeTab.value);
-}
+let myTab = ref(null);
 
-//监听当前tab界面，发生改变时自动切换路由
-watch(activeTab, (newValue) => {
-  router.push(newValue);
-});
+//初始化函数
+function init(){}
 
 //点击侧边栏菜单
 function handleSelect(menu){
-
-  // 检查这个路由是否已经在 tabList 中
-  if (!tabList.value.some(tab => tab.path === menu.key)) {
-    // 如果不在，就添加到 tabList 中
-    tabList.value.push({
-      label: menu.name, // 这只是一个示例，你应该根据你的实际情况来设置标签的名称
-      path: menu.key,
-      closable: true, // 这些 tab 可以被关掉
-    });
-  }
-  // 设置这个路由为当前活动的 tab
-  activeTab.value = menu.key;
+  myTab.value.addTab(menu.name, menu.key)
 }
-
-//删除tab
-function removeTab(currentTab){
-  let tabs = tabList.value;
-  let a = activeTab.value;
-  //如果当前路径和你要删除的路径一致
-  if (currentTab === a) {
-    tabs.forEach((tab, index) => {
-      //要删除的路径和tabList的路径匹配
-      if (tab.path === currentTab) {
-        const nextTab = tabs[index + 1] || tabs[index - 1];
-        //如果存在，获取nextTab路径
-        if (nextTab) {
-          a = nextTab.path;
-        }
-      }
-    });
-  }
-  //重新赋值activeTab
-  activeTab.value = a;
-  //过滤删除后的tabList
-  tabList.value = tabList.value.filter((tab) => tab.path !== currentTab);
-}
-
 </script>
 
 <style scoped>
