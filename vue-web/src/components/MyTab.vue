6bd04919bf04478f6adadb7bd1bc76070d20f923
CM-Silence <757902252@qq.com>
2024-03-29 22:27:39 +0800

    [new]tab界面需要频繁使用，做成子组件

---
+++ vue-web/src/components/MyTab.vue
@@ -0,0 +1,93 @@
+<template>
+  <el-tabs
+      v-model="activeTab"
+      type="card"
+      @tab-remove="removeTab"
+      :closable="tabList.length > 1"
+  >
+    <el-tab-pane
+        v-for="item in tabList"
+        :label="item.label"
+        :name="item.path"
+    >
+      <router-view></router-view>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script setup>
+
+import {router} from "@/router/index.js";
+import {onMounted, ref, watch} from "vue";
+
+const prop = defineProps({
+  defaultTab: {
+    type: Object,
+    default: () => '',
+    description: '默认tab界面对象, 传参时可当做是初始显示界面的路由'+
+        '\n对象格式为：  {label: "标签名", path: "标签路由"}'
+  }
+});
+
+const tabList = ref([prop.defaultTab]);
+const activeTab = ref(prop.defaultTab.path)
+
+//暴露函数，可供父组件调用
+defineExpose({
+  addTab
+});
+
+onMounted(init)
+
+//初始化函数
+function init(){
+  router.push(activeTab.value);
+}
+
+//监听当前tab界面，发生改变时自动切换路由
+watch(() => activeTab.value, (newValue) => {
+  router.push(newValue);
+});
+
+//添加tab界面
+function addTab(name, path){
+  // 检查这个路由是否已经在 tabList 中
+  if (!tabList.value.some(tab => tab.path === path)) {
+    // 如果不在，就添加到 tabList 中
+    tabList.value.push({
+      label: name, // 这只是一个示例，你应该根据你的实际情况来设置标签的名称
+      path: path,
+      closable: true, // 这些 tab 可以被关掉
+    });
+  }
+  // 设置这个路由为当前活动的 tab
+  activeTab.value = path;
+}
+
+//删除tab
+function removeTab(currentTab){
+  let tabs = tabList.value;
+  let a = activeTab.value;
+  //如果当前路径和你要删除的路径一致
+  if (currentTab === a) {
+    tabs.forEach((tab, index) => {
+      //要删除的路径和tabList的路径匹配
+      if (tab.path === currentTab) {
+        const nextTab = tabs[index + 1] || tabs[index - 1];
+        //如果存在，获取nextTab路径
+        if (nextTab) {
+          a = nextTab.path;
+        }
+      }
+    });
+  }
+  //重新赋值activeTab
+  activeTab.value = a;
+  //过滤删除后的tabList
+  tabList.value = tabList.value.filter((tab) => tab.path !== currentTab);
+}
+</script>
+
+<style scoped>
+
+</style>
