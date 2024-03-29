8d7e3dfd5ceb37b271020737cfc4676b2b7f393f
LanceHE6 <2765543491@qq.com>
2024-03-29 11:00:54 +0800

    refactor.增加路由文件管理路由

--- go-server/main.go
+++ go-server/main.go
@@ -1,14 +1,7 @@
 package main
 
 import (
-	"Go_simpleWMS/handler/auth"
-	"Go_simpleWMS/handler/department"
-	"Go_simpleWMS/handler/goodsType"
-	"Go_simpleWMS/handler/staff"
-	"Go_simpleWMS/handler/test"
-	"Go_simpleWMS/handler/upload"
-	"Go_simpleWMS/handler/user"
-	"Go_simpleWMS/handler/warehouse"
+	"Go_simpleWMS/route"
 	"Go_simpleWMS/utils"
 	"fmt"
 	"github.com/gin-contrib/cors"
@@ -51,109 +44,7 @@ func main() {
 		MaxAge: 24 * time.Hour,
 	}))
 
-	// 设置静态资源路径
-	ginServer.Static("/res", "./static/res")
-
-	ginServer.GET("/ping", func(context *gin.Context) {
-		test.Ping(context)
-	})
-	// 鉴权接口
-	ginServer.GET("/auth", utils.AuthMiddleware(), func(context *gin.Context) {
-		auth.AuthByHeader(context)
-	})
-	ginServer.POST("/upload", func(context *gin.Context) {
-		upload.UploadFile(context)
-	})
-	// 路由分组
-	userGroup := ginServer.Group("/user")
-
-	userGroup.POST("/register", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		user.Register(context)
-	})
-	userGroup.POST("/login", func(context *gin.Context) {
-		user.Login(context)
-	})
-	userGroup.DELETE("/delete", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		user.DeleteUser(context)
-	})
-	userGroup.PUT("/update", utils.AuthMiddleware(), func(context *gin.Context) {
-		user.UpdateUser(context)
-	})
-	userGroup.GET("/list", utils.AuthMiddleware(), func(context *gin.Context) {
-		user.ListUsers(context)
-	})
-
-	warehouseGroup := ginServer.Group("/warehouse")
-
-	warehouseGroup.POST("/add", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		warehouse.AddWarehouse(context)
-	})
-
-	warehouseGroup.DELETE("/delete", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		warehouse.DeleteWarehouse(context)
-	})
-
-	warehouseGroup.PUT("/update", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		warehouse.UpdateWarehouse(context)
-	})
-
-	warehouseGroup.GET("/list", utils.AuthMiddleware(), func(context *gin.Context) {
-		warehouse.ListWarehouse(context)
-	})
-
-	goodsTypeGroup := ginServer.Group("/gt")
-
-	goodsTypeGroup.POST("/add", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		goodsType.AddGoodsType(context)
-	})
-
-	goodsTypeGroup.PUT("/update", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		goodsType.UpdateGoodsType(context)
-	})
-
-	goodsTypeGroup.DELETE("/delete", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		goodsType.DeleteGoodsType(context)
-	})
-
-	goodsTypeGroup.GET("/list", utils.AuthMiddleware(), func(context *gin.Context) {
-		goodsType.ListGoodsType(context)
-	})
-
-	departmentGroup := ginServer.Group("/dept")
-
-	departmentGroup.POST("/add", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		department.AddDepartment(context)
-	})
-
-	departmentGroup.PUT("/update", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		department.UpdateDepartment(context)
-	})
-
-	departmentGroup.DELETE("/delete", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		department.DeleteDepartment(context)
-	})
-
-	departmentGroup.GET("/list", utils.AuthMiddleware(), func(context *gin.Context) {
-		department.ListDepartment(context)
-	})
-
-	staffGroup := ginServer.Group("/staff")
-
-	staffGroup.POST("/add", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		staff.AddStaff(context)
-	})
-
-	staffGroup.PUT("/update", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		staff.UpdateStaff(context)
-	})
-
-	staffGroup.DELETE("/delete", utils.AuthMiddleware(), utils.IsSuperAdminMiddleware(), func(context *gin.Context) {
-		staff.DeleteStaff(context)
-	})
-
-	staffGroup.GET("/list", utils.AuthMiddleware(), func(context *gin.Context) {
-		staff.ListStaff(context)
-	})
+	route.Route(ginServer)
 
 	err := ginServer.Run(":8080")
 	if err != nil {
