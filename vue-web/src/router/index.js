2c9a218d2e435456b168ba5aa499be38cfdcaf24
CM-Silence <757902252@qq.com>
2024-03-29 18:07:15 +0800

    [new]新增出入库管理菜单的四个菜单的路由

--- vue-web/src/router/index.js
+++ vue-web/src/router/index.js
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue';
-import Main from  '../views/Main.vue'
-import ProductManagement from  '../views/main-views/ProductManagement.vue'
-import EntryAndOut from  '../views/main-views/EntryAndOut.vue'
 
+import Main from  '../views/Main.vue'
+import HomePage from  '../views/main-views/HomePage.vue'
+
+import ProductManagement from  '../views/main-views/ProductManagement.vue'
+
+import EntryAndOut from  '../views/main-views/EntryAndOut.vue'
+import Entry from '../views/main-views/entry-and-out/Entry.vue'
+import Out from '../views/main-views/entry-and-out/Out.vue'
+import Check from '../views/main-views/entry-and-out/Check.vue'
+import Allocate from '../views/main-views/entry-and-out/Allocate.vue'
+
+
+import Setting from  '../views/main-views/Setting.vue'
 
 
 
@@ -11,9 +21,16 @@ const routes = [
     { path: '/', component: Login , name: 'login'},
     { path: '/home', component: Main , name: 'home',
         children: [
-            // 二级路由
+            { path: '/home/homePage', component: HomePage , name: 'homePage'},
             { path: '/home/productManagement', component: ProductManagement , name: 'productManagement'},
-            { path: '/home/entryAndOut', component: EntryAndOut , name: 'entryAndOut'},
+            { path: '/home/entryAndOut', component: EntryAndOut , name: 'entryAndOut',
+                children:[
+                    { path: '/home/entryAndOut/entry', component: Entry , name: 'entry'},
+                    { path: '/home/entryAndOut/out', component: Out , name: 'out'},
+                    { path: '/home/entryAndOut/check', component: Check , name: 'check'},
+                    { path: '/home/entryAndOut/allocate', component: Allocate , name: 'allocate'},
+                ]},
+            { path: '/home/setting', component: Setting , name: 'setting'},
         ]
     },
 ];
