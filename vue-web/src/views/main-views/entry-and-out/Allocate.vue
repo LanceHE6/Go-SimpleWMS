478b56509fb577706dfec05db4a6ec3f01d4aa7d
CM-Silence <757902252@qq.com>
2024-03-29 18:02:35 +0800

    [new]新增出入库管理四个子页面

---
+++ vue-web/src/views/main-views/entry-and-out/Allocate.vue
@@ -0,0 +1,11 @@
+<template>
+调拨单
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-web/src/views/main-views/entry-and-out/Entry.vue
@@ -0,0 +1,11 @@
+<template>
+入库单
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
