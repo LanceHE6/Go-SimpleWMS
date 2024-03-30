088899d59109a951b18ca8625ffdda1ca7dce2f5
LanceHE6 <2765543491@qq.com>
2024-03-30 19:51:24 +0800

    fix.完善鉴权接口逻辑

--- go-server/handler/auth/auth.go
+++ go-server/handler/auth/auth.go
@@ -8,12 +8,33 @@ import (
 
 func AuthByHeader(context *gin.Context) {
 
-	uid, err := utils.GetUidByContext(context)
+	uid, _, registerTime, err := utils.GetUserInfoByContext(context)
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{
 			"message": "Invalid token",
 			"code":    401,
 		})
+		return
+	}
+	// 判断是否在数据库中
+	tx, _ := utils.GetDbConnection()
+	var isExist int
+	err = tx.QueryRow("SELECT count(*) from user where uid=? and register_time=?", uid, registerTime).Scan(&isExist)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"error":  "Cannot get the number of uid for this uid",
+			"detail": err.Error(),
+			"code":   501,
+		})
+		context.Abort()
+		return
+	}
+	if isExist <= 0 {
+		context.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Invalid token",
+			"code":    101,
+		})
+		context.Abort()
 		return
 	}
 	context.JSON(http.StatusOK, gin.H{
