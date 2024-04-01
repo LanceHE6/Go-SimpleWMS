5f2eb0b42d48b487a3fdb8d69a253d0cf576e546
CM-Silence <757902252@qq.com>
2024-04-01 21:37:36 +0800

    [new]新增界面路由

--- vue-web/src/router/index.js
+++ vue-web/src/router/index.js
@@ -15,25 +15,34 @@ import Allocate from '../views/main-views/entry-and-out/Allocate.vue'
 
 import Setting from  '../views/main-views/Setting.vue'
 import UserManagement from '../views/main-views/setting/UserManagement.vue'
-
+import WarehouseManagement from '../views/main-views/setting/WarehouseManagement.vue'
+import GoodsTypeManagement from '../views/main-views/setting/GoodsTypeManagement.vue'
+import DepartmentManagement from '../views/main-views/setting/DepartmentManagement.vue'
+import StaffManagement from '../views/main-views/setting/StaffManagement.vue'
+import InventoryTypeManagement from '../views/main-views/setting/InventoryTypeManagement.vue'
 
 const routes = [
     { path: '/', component: Login , name: 'login'},
-    { path: '/home', component: Main , name: 'home',
+    { path: '/home', component: Main , name: 'home', meta: { requiresAuth: true, beforeName:'login' },
         children: [
-            { path: '/home/homePage', component: HomePage , name: 'homePage'},
-            { path: '/home/productManagement', component: ProductManagement , name: 'productManagement'},
-            { path: '/home/entryAndOut', component: EntryAndOut , name: 'entryAndOut',
+            { path: '/home/homePage', component: HomePage , name: 'homePage', meta: { beforeName:'main' }},
+            { path: '/home/productManagement', component: ProductManagement , name: 'productManagement', meta: { beforeName:'main' }},
+            { path: '/home/entryAndOut', component: EntryAndOut , name: 'entryAndOut', meta: { beforeName:'main' },
                 children:[
-                    { path: '/home/entryAndOut/entry', component: Entry , name: 'entry'},
+                    { path: '/home/entryAndOut/entry', component: Entry , name: 'entry', meta: { beforeName:'entryAndOut' }},
                     { path: '/home/entryAndOut/out', component: Out , name: 'out'},
                     { path: '/home/entryAndOut/check', component: Check , name: 'check'},
                     { path: '/home/entryAndOut/allocate', component: Allocate , name: 'allocate'},
                 ]
             },
-            { path: '/home/setting', component: Setting , name: 'setting',
+            { path: '/home/setting', component: Setting , name: 'setting', meta: { beforeName:'main' },
                 children:[
                     { path: '/home/setting/userManagement', component: UserManagement , name: 'userManagement'},
+                    { path: '/home/setting/warehouseManagement', component: WarehouseManagement , name: 'warehouseManagement'},
+                    { path: '/home/setting/goodsTypeManagement', component: GoodsTypeManagement , name: 'goodsTypeManagement'},
+                    { path: '/home/setting/departmentManagement', component: DepartmentManagement , name: 'departmentManagement'},
+                    { path: '/home/setting/staffManagement', component: StaffManagement , name: 'sStaffManagement'},
+                    { path: '/home/setting/inventoryTypeManagement', component: InventoryTypeManagement , name: 'inventoryTypeManagement'},
                 ]
             },
         ]
