3d8e3c642337b19a8bd71e4d81dff2894e69b21d
CM-Silence <757902252@qq.com>
2024-04-09 00:38:00 +0800

    [refactor]将网络请求部分放到了子组件

--- vue-web/src/views/main-views/setting/WarehouseManagement.vue
+++ vue-web/src/views/main-views/setting/WarehouseManagement.vue
@@ -1,68 +1,33 @@
 <template>
-  <my-table
-      ref="myTable"
-      :table-col-list="tableColList"
-      :default-data="data.warehouseArray"
-      :search-data="'name'"
-      :add-data-template="addForm"
-      :edit-data-template="editForm"
-      :loading="data.isLoading"
-      @add="add"
-      @upload="upload"
-      @del="del"
-      @edit="edit"
-  >
-  </my-table>
+  <DataShowView
+    :table-col-list="tableColList"
+    :key-data="'wid'"
+    :search-data="'name'"
+    :add-form="addForm"
+    :edit-form="editForm"
+    :delete-data-body="deleteDataBody"
+    :urls="urls"
+  />
 </template>
 
 <script setup>
-import axios from "axios";
-import {ElMessage, ElNotification} from "element-plus";
-import MyTable from "@/components/MyTable.vue";
-import {onMounted, reactive} from "vue";
+import {reactive} from "vue";
+import DataShowView from "@/components/DataShowView.vue";
 
-//表头属性列表
+/**
+ * 表头属性列表
+ * */
 const tableColList = [
   {property: "name", label: "仓库名", sortable: false},
   {property: "comment", label: "备注", sortable: false},
   {property: "manager", label: "管理人", sortable: false},
   {property: "status", label: "状态", sortable: false},
-  {property: "add_time", label: "创建时间", sortable: true},
-  {property: "wid", label: "仓库ID", sortable: true, width: 100},
+  {property: "created_at", label: "创建时间", sortable: true},
+  {property: "wid", label: "仓库ID", sortable: true, width: 120},
 ]
 
-//初始化函数
-onMounted(() => {
-  warehouseList()
-})
-
-//点击子组件的添加按钮, 子组件处理完返回的可提交表单
-function add(form){
-  warehouseAdd(form)
-}
-
-//点击子组件的上传按钮, 子组件处理完返回的可提交表单
-function upload(form){
-  warehouseUpload(form)
-}
-
-//点击子组件的删除按钮
-function del(row){
-  warehouseDelete(row.uid)
-}
-
-//点击子组件的编辑按钮, 子组件处理完返回的可提交表单
-function edit(form){
-  warehouseUpdate(form)
-}
-
-const data =  reactive({
-  warehouseArray: [],  //接收用户对象的列表
-  isLoading: true  //是否正在加载
-})
-
 /**
- * 编辑用户时所用到的表单对象
+ * 编辑仓库时所用到的表单对象
  * */
 const editForm = reactive({
   data : {
@@ -91,14 +56,19 @@ const editForm = reactive({
         {label: '禁用', value: 0},
         {label: '启用', value: 1},
       ]},
-    {label: '管理人', prop: 'manager', dataName: 'manager', isInput: true},
+    {label: '管理人', prop: 'manager', dataName: 'manager', isFK: true,
+      FKData:{
+        url: "/staff/list",
+        property: "sid",
+        label: "name"
+      }},
     {label: '备注', prop: 'comment', dataName: 'comment', isInput: true},
   ],
   key: 'wid'
 })
 
 /**
- * 注册用户时所用到的对象
+ * 添加仓库时所用到的对象
  * */
 const addForm = reactive({
   data :{
@@ -106,6 +76,12 @@ const addForm = reactive({
     comment:'',
     manager:'',
     status:'',
+  },
+  dataType:{
+    name:'String',
+    comment:'String',
+    manager:'String',
+    status:'Int',
   },
   dataNum: 4,
   rules: {
@@ -123,168 +99,36 @@ const addForm = reactive({
   item:[
     {label: '仓库名', prop: 'name', dataName: 'name', isInput: true},
     {label: '备注', prop: 'comment', dataName: 'comment', isInput: true},
-    {label: '管理人', prop: 'manager', dataName: 'manager', isInput: true},
     {label: '仓库状态', prop: 'status', dataName: 'status', isSelect: true,
       selectOptions: [
         {label: '禁用', value: 0},
         {label: '启用', value: 1},
       ]},
+    {label: '管理人', prop: 'manager', dataName: 'manager', isFK: true,
+      FKData:{
+        url: "/staff/list",
+        property: "sid",
+        label: "name"
+      }},
   ],
 })
 
-const token="bearer "+localStorage.getItem("token");
-
 /**
- * warehouseList()
- * 获取仓库的信息的请求
- * 打开网页时自动调用一次
- * 结果：warehouseArray中包含一到多个warehouse对象
+ * 删除请求体
  * */
-
-const warehouseList = async () => {
-  data.isLoading = true
-  await axios.get('/warehouse/list', {
-    headers: {
-      'Authorization': token
-    }
-  })
-      .then(result => {
-        console.log("warehouseList:", result)
-        if (result && result.data) {
-          for (let i = 0; i < result.data.rows.length; i++){
-            result.data.rows[i].add_time = new Date(Number(result.data.rows[i].add_time) * 1000).toLocaleString()
-          }
-          data.warehouseArray = result.data.rows;
-        }
-      })
-      .catch(error => {
-        ElMessage.error("网络请求出错了！")
-        console.error("warehouseList:", error)
-      })
-  data.isLoading = false
+const deleteDataBody = {
+  wid: ""
 }
 
 /**
- * warehouseDelete()
- * 删除仓库的请求，param: wid 类型:String
+ * 网络请求url
  * */
-const warehouseDelete=async (wid) => {
-  data.isLoading = true
-  await axios.delete('/warehouse/delete', {
-        headers: {
-          'Authorization': token
-        },
-        data: {
-          "wid": wid
-        }
-      }
-  )
-      .then( async message => {
-        ElMessage.success("仓库已被删除！")
-        console.log("warehouseDelete:", message)
-        await warehouseList()
-      })
-      .catch( error => {
-        ElMessage.error("网络请求出错了！")
-        console.error("warehouseDelete:", error)
-      })
-  data.isLoading = false
-}
-
-/**
- * warehouseAdd()
- * 新增仓库 param: warehouse对象
- * warehouse={
- *     name:'',
- *     comment:'',
- *     manager:'',
- *     status:0,
- * }
- * */
-const warehouseAdd=async (warehouse) => {
-  data.isLoading = true
-  await axios.post('/warehouse/add', warehouse, {
-    headers: {
-      'Authorization': token
-    }
-  })
-      .then( async message => {
-        ElMessage.success("仓库添加成功！")
-        console.log("warehouseAdd:", message)
-        await warehouseList()
-      })
-      .catch( error => {
-        ElMessage.error("网络请求出错了！仓库名是否已被定义？")
-        console.error("warehouseAdd:", error)
-      })
-  data.isLoading = false
-}
-
-/**
- * warehouseUpdate()
- * 修改仓库  param: warehouse对象
- * warehouse={
- *     wid:'',
- *     name:'',
- *     comment:'',
- *     manager:'',
- *     status:0,
- * }
- * */
-const warehouseUpdate=async (warehouse) => {
-  data.isLoading = true
-  await axios.put('/warehouse/update', warehouse, {
-        headers: {
-          'Authorization': token
-        },
-      }
-  ).then( async message => {
-    ElMessage.success("仓库信息修改成功！")
-    console.log("warehouseUpdate:", message)
-    await warehouseList()
-  })
-      .catch( error => {
-        ElMessage.error("网络请求出错了！")
-        console.error("warehouseUpdate:", error)
-      })
-  data.isLoading = false
-}
-
-const warehouseUpload=async (list) => {
-  console.log(list)
-  data.isLoading = true
-  await axios.post('/warehouse/register-batch', {warehouse_list: list}, {
-    headers: {
-      'Authorization': token
-    }
-  })
-      .then( async message => {
-        if(message.status === 200) {
-          ElMessage.success("批量导入成功！")
-        }
-        else if(message.status === 217){
-          ElMessage.warning("部分导入成功")
-          let errMessage = ""
-          for(const item in message.data.detail){
-            if(message.data.detail[item].code === 402){
-              errMessage += (message.data.detail[item].message + "\n")
-              ElNotification({
-                title: '失败信息',
-                message: message.data.detail[item].message,
-                type: 'error',
-                duration: 30000,
-              })
-            }
-          }
-        }
-        console.log("warehouseUpload:", message)
-        await warehouseList()
-      })
-      .catch( error => {
-        ElMessage.error("网络请求出错了！仓库名是否已被定义？")
-        console.error("warehouseUpload:", error)
-      })
-  data.isLoading = false
+const urls = {
+  getData: "/warehouse/list",
+  deleteData: "/warehouse/delete",
+  addData: "/warehouse/add",
+  updateData: "/warehouse/update",
+  uploadData: "/warehouse/upload",
 }
 </script>
 
