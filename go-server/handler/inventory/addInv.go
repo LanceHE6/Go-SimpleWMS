c33834a144ab9ac8208bcfb189dfdf46b0a89345
LanceHE6 <2765543491@qq.com>
2024-05-21 12:00:56 +0800

    fix.修复参数名错误 fix.修复单号构建错误 feat.新增无gid参数自动创建货品并添加出入库单 #docker-push

--- go-server/handler/inventory/addInv.go
+++ go-server/handler/inventory/addInv.go
@@ -6,16 +6,16 @@ import (
 	"Go_simpleWMS/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"strconv"
 	"time"
 )
 
 type addInvRequest struct {
-	Gid          string `json:"gid" form:"gid" binding:"required"`
+	Gid          string `json:"gid" form:"gid"`
+	Name         string `json:"name" form:"name"`
 	Amount       int    `json:"amount" form:"amount" binding:"required"`
 	InventoryTpe string `json:"inventory_type" form:"inventory_type" binding:"required"`
-	Wid          string `json:"wid" form:"wid" binding:"required"`
-	Sid          string `json:"sid" form:"sid" binding:"required"`
+	Warehouse    string `json:"warehouse" form:"warehouse" binding:"required"`
+	Operator     string `json:"operator" form:"operator" binding:"required"`
 	Comment      string `json:"comment" form:"comment"`
 	Manufacturer string `json:"manufacturer" form:"manufacturer"`
 }
@@ -31,37 +31,83 @@ func AddInv(context *gin.Context) {
 		return
 	}
 	Gid := data.Gid
+	Name := data.Name
 	Amount := data.Amount
 	InventoryType := data.InventoryTpe
-	Wid := data.Wid
-	Sid := data.Sid
+	Wid := data.Warehouse
+	Sid := data.Operator
 	Comment := data.Comment
 	Manufacturer := data.Manufacturer
 
-	Iid := "i" + utils.GenerateUuid(8) // 转换为 8 位字符串
-
 	db := myDb.GetMyDbConnection()
-	var goods model.Goods
-	err := db.Model(&model.Goods{}).Where("gid=?", Gid).First(&goods).Error
+
+	var iType model.InventoryType
+	err := db.Model(&model.InventoryType{}).Where("itid=?", InventoryType).First(&iType).Error
 	if err != nil {
-		context.JSON(http.StatusForbidden, gin.H{
-			"message": "The goods does not exist",
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "The inventory type does not exist",
 			"code":    402,
 		})
 		return
 	}
+
+	Iid := "i" + utils.GenerateUuid(8) // 转换为 8 位字符串
+
+	var goods model.Goods
+	err = db.Model(&model.Goods{}).Where("gid=?", Gid).First(&goods).Error
+	if err != nil {
+		// 该货品不存在 判断出入库类型是否为入库
+		// 1 为入库 2 为出库
+		if iType.Type != 1 {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"message": "The goods does not exist",
+				"code":    403,
+			})
+			return
+		}
+		// 生成新的货品
+		newGid := "g" + utils.GenerateUuid(8) // 转换为 8 位字符串
+		if Name == "" {
+			Name = "新添加货品"
+		}
+
+		goods = model.Goods{
+			Gid:          newGid,
+			Name:         Name,
+			GoodsType:    "_default_",
+			Warehouse:    Wid,
+			Manufacturer: Manufacturer,
+			Unit:         "_default_",
+			Quantity:     0,
+		}
+		err = db.Create(&goods).Error
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{
+				"error":  "Cannot insert the goods",
+				"detail": err.Error(),
+				"code":   501,
+			})
+			return
+		}
+	}
+
 	goodsCode := goods.GoodsCode
 	if goodsCode == "" {
 		goodsCode = "G" + utils.GenerateUuid(4)
 	}
 
 	// 构建单号
-	nowTime, _ := time.Parse("20060102150405", strconv.FormatInt(time.Now().Unix(), 10))
-	Number := nowTime.String() + goodsCode
+	nowTime := time.Now().Format("200601021504")
+	Number := ""
+	if iType.Type == 1 {
+		Number = "I" + nowTime + goodsCode
+	} else {
+		Number = "O" + nowTime + goodsCode
+	}
 
 	var inventory = model.Inventory{
 		Iid:           Iid,
-		Goods:         Gid,
+		Goods:         goods.Gid,
 		Amount:        Amount,
 		Number:        Number,
 		InventoryType: InventoryType,
@@ -81,7 +127,12 @@ func AddInv(context *gin.Context) {
 		return
 	}
 	// 更新货品表
-	goods.Quantity += Amount
+	if iType.Type == 1 {
+		goods.Quantity += Amount
+	} else {
+		goods.Quantity -= Amount
+	}
+
 	err = db.Model(model.Goods{}).Where("gid=?", goods.Gid).Updates(&goods).Error
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{
