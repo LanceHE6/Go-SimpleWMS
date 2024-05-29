eebaad0f0ce650721c97dd40395cdfe297ee69e5
CM-Silence <757902252@qq.com>
2024-05-29 18:54:32 +0800

    [update]配置文件不存在时创建默认配置文件

--- vue-web/src/main.js
+++ vue-web/src/main.js
@@ -3,11 +3,29 @@ import { router } from './router/index'
 import App from './App.vue'
 import ElementUI from 'element-plus'
 import axios from "axios"
+import config from '../config.json';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import "element-plus/theme-chalk/el-message.css";
+import * as path from "path";
+import * as fs from "fs";
 
-axios.defaults.baseURL = localStorage.getItem("url") || "http://47.236.80.244:6007/api";
+// 定义默认配置
+const defaultConfig = {
+    host: "47.236.80.244",
+    port: "6007",
+    path: "api"
+};
+
+// 定义配置文件的路径
+const configPath = path.resolve(__dirname, '../config.json');
+
+// 检查文件是否存在
+if (!fs.existsSync(configPath)) {
+    // 如果文件不存在，写入默认配置
+    fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
+}
+axios.defaults.baseURL = "http://" + config.host + ":" + config.port + "/" + config.path;
 
 const app = createApp(App)
 
