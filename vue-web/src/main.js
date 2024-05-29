e81af57ada9158703654d08ab0129763a3fedc23
CM-Silence <757902252@qq.com>
2024-05-29 16:38:55 +0800

    [new]添加配置文件，方便管理

--- vue-web/src/main.js
+++ vue-web/src/main.js
@@ -3,28 +3,11 @@ import { router } from './router/index'
 import App from './App.vue'
 import ElementUI from 'element-plus'
 import axios from "axios"
-import config from '../config.json';
+import config from './config.json';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import "element-plus/theme-chalk/el-message.css";
-import path from "path";
-import fs from "fs";
 
-// 定义默认配置
-const defaultConfig = {
-    host: "47.236.80.244",
-    port: "6007",
-    path: "api"
-};
-
-// 定义配置文件的路径
-const configPath = path.resolve(__dirname, '../config.json');
-
-// 检查文件是否存在
-if (!fs.existsSync(configPath)) {
-    // 如果文件不存在，写入默认配置
-    fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
-}
 
 axios.defaults.baseURL = "http://" + config.host + ":" + config.port + "/" + config.path;
 
