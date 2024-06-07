8847a589c0c85f167d64f803d3da6619b2832bad
LanceHE6 <2765543491@qq.com>
2024-06-07 15:45:05 +0800

    refactor.重构出入库表添加

--- go-server/handler/inventory/addInv.go
+++ go-server/handler/inventory/addInv.go
@@ -6,6 +6,7 @@ import (
 	"Go_simpleWMS/utils"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	"net/http"
 	"time"
 )
@@ -15,6 +16,7 @@ type addInvRequest struct {
 	Number       string `json:"number" form:"number"`                                    // 单号
 	Department   string `json:"department" form:"department"`                            // 单据所属部门
 	GoodsList    string `json:"goods_list" form:"goods_list" binding:"required"`         // 单据包含的货品
+	Warehouse    string `json:"warehouse" form:"warehouse" binding:"required"`           // 仓库
 	InventoryTpe string `json:"inventory_type" form:"inventory_type" binding:"required"` // 出入库类型
 	Operator     string `json:"operator" form:"operator" binding:"required"`             // 经办人
 	Comment      string `json:"comment" form:"comment"`                                  // 备注
@@ -34,6 +36,7 @@ func AddInv(context *gin.Context) {
 	Date := data.Date
 	Number := data.Number
 	Department := data.Department
+	Warehouse := data.Warehouse
 	var GoodsList model.GoodsList
 	err := json.Unmarshal([]byte(data.GoodsList), &GoodsList)
 	if err != nil {
@@ -68,9 +71,9 @@ func AddInv(context *gin.Context) {
 	if Number == "" {
 		nowTime := time.Now().Format("200601021504")
 		if iType.Type == 1 {
-			Number = "I" + nowTime
+			Number = "RK" + nowTime + utils.GenerateUuid(4)
 		} else {
-			Number = "O" + nowTime
+			Number = "CK" + nowTime + utils.GenerateUuid(4)
 		}
 	}
 
@@ -96,6 +99,7 @@ func AddInv(context *gin.Context) {
 		Date:          parsedDate,
 		Department:    Department,
 		GoodsList:     GoodsList,
+		Warehouse:     Warehouse,
 		Number:        Number,
 		InventoryType: InventoryType,
 		Operator:      Sid,
@@ -103,6 +107,8 @@ func AddInv(context *gin.Context) {
 		Manufacturer:  Manufacturer,
 	}
 
+	UpdateStocks(GoodsList, Warehouse, iType, context, db)
+	// 插入单据
 	err = db.Model(model.Inventory{}).Create(&inventory).Error
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{
@@ -112,29 +118,58 @@ func AddInv(context *gin.Context) {
 		})
 		return
 	}
-	// 更新货品表
-	for _, goodsOrder := range GoodsList {
-		var goods model.Goods
-		db.Model(model.Goods{}).Where("gid = ?", goodsOrder.Goods).First(&goods)
-		if iType.Type == 1 {
-			goods.Quantity += goodsOrder.Amount
-		} else {
-			goods.Quantity -= goodsOrder.Amount
-		}
-
-		err = db.Model(model.Goods{}).Where("gid=?", goodsOrder.Goods).Updates(&goods).Error
-		if err != nil {
-			context.JSON(http.StatusInternalServerError, gin.H{
-				"error":  "Cannot update goods",
-				"code":   502,
-				"detail": err.Error(),
-			})
-			return
-		}
-
-	}
 	context.JSON(http.StatusOK, gin.H{
 		"message": "Inventory added successfully",
 		"code":    201,
 	})
+}
+
+func UpdateStocks(GoodsList model.GoodsList, Warehouse string, inventoryType model.InventoryType, context *gin.Context, db *gorm.DB) int {
+	// 更新库存表
+	for _, goodsOrder := range GoodsList {
+		var stock model.Stock
+		// 判断库存表中是否存在这个映射
+		notExist := db.Model(model.Stock{}).Where("warehouse=? AND goods=?", Warehouse, goodsOrder.Goods).First(&stock).RecordNotFound()
+		var newStock = model.Stock{
+			Warehouse: Warehouse,
+			Goods:     goodsOrder.Goods,
+			Quantity:  goodsOrder.Amount,
+		}
+		// 根据出入库方向执行不同的操作
+		if inventoryType.Type == 1 {
+			if notExist {
+				// 不存在就插入记录新增映射
+				db.Model(model.Stock{}).Create(&newStock)
+			} else {
+				// 存在就更新记录
+				db.Model(model.Stock{}).Where("warehouse=? AND goods=?", Warehouse, goodsOrder.Goods).Update("quantity", stock.Quantity+goodsOrder.Amount)
+			}
+		} else {
+			if notExist {
+				// 无仓库货品映射，无法执行出库操作
+				context.JSON(http.StatusBadRequest, gin.H{
+					"message": "The goods is not included in the warehouse and cannot be outbound",
+					"code":    405,
+					"detail":  "",
+				})
+				return 1
+			} else {
+				// 存在就更新记录
+				if stock.Quantity < goodsOrder.Amount {
+					context.JSON(http.StatusBadRequest, gin.H{
+						"message": "Including outbound goods with insufficient stock",
+						"code":    406,
+						"detail":  "",
+					})
+					return 1
+				} else {
+					var updateData = map[string]interface{}{
+						"quantity": stock.Quantity - goodsOrder.Amount,
+					}
+					db.Model(model.Stock{}).Where("warehouse=? AND goods=?", Warehouse, goodsOrder.Goods).Updates(updateData)
+				}
+			}
+		}
+	}
+	return 0
 }
