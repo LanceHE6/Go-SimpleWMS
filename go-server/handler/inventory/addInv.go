502e1389b3abd26802331a1141f8274c46cfb602
LanceHE6 <2765543491@qq.com>
2024-06-07 17:48:16 +0800

    chaos.优化代码逻辑

--- go-server/handler/inventory/addInv.go
+++ go-server/handler/inventory/addInv.go
@@ -3,10 +3,10 @@ package inventory
 import (
 	"Go_simpleWMS/database/model"
 	"Go_simpleWMS/database/myDb"
+	"Go_simpleWMS/handler/stock"
 	"Go_simpleWMS/utils"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	"net/http"
 	"time"
 )
@@ -93,12 +93,34 @@ func AddInv(context *gin.Context) {
 			return
 		}
 	}
+	// 构造更新前后库存数据
+	var oldGoodsList model.GoodsList
+	var newGoodsList model.GoodsList
+	for _, g := range GoodsList {
+		var oldGoodsOrder model.GoodsOrder
+		var newGoodsOrder model.GoodsOrder
+		oldStock := stock.GetStock(Warehouse, g.Goods)
+		oldGoodsOrder.Goods = g.Goods
+		newGoodsOrder.Goods = g.Goods
+		oldGoodsOrder.Amount = oldStock
+		if iType.Type == 1 {
+			newGoodsOrder.Amount = oldStock + g.Amount
+		} else {
+			newGoodsOrder.Amount = oldStock - g.Amount
+		}
+		oldGoodsList = append(oldGoodsList, oldGoodsOrder)
+		newGoodsList = append(newGoodsList, newGoodsOrder)
+	}
+	// 更新库存
+	UpdateStocks(GoodsList, Warehouse, iType, context, db)
 
 	var inventory = model.Inventory{
 		Iid:           Iid,
 		Date:          parsedDate,
 		Department:    Department,
 		GoodsList:     GoodsList,
+		OldGoodsList:  oldGoodsList,
+		NewGoodsList:  newGoodsList,
 		Warehouse:     Warehouse,
 		Number:        Number,
 		InventoryType: InventoryType,
@@ -106,8 +128,6 @@ func AddInv(context *gin.Context) {
 		Comment:       Comment,
 		Manufacturer:  Manufacturer,
 	}
-
-	UpdateStocks(GoodsList, Warehouse, iType, context, db)
 	// 插入单据
 	err = db.Model(model.Inventory{}).Create(&inventory).Error
 	if err != nil {
@@ -122,54 +142,4 @@ func AddInv(context *gin.Context) {
 		"message": "Inventory added successfully",
 		"code":    201,
 	})
-}
-
-func UpdateStocks(GoodsList model.GoodsList, Warehouse string, inventoryType model.InventoryType, context *gin.Context, db *gorm.DB) int {
-	// 更新库存表
-	for _, goodsOrder := range GoodsList {
-		var stock model.Stock
-		// 判断库存表中是否存在这个映射
-		notExist := db.Model(model.Stock{}).Where("warehouse=? AND goods=?", Warehouse, goodsOrder.Goods).First(&stock).RecordNotFound()
-		var newStock = model.Stock{
-			Warehouse: Warehouse,
-			Goods:     goodsOrder.Goods,
-			Quantity:  goodsOrder.Amount,
-		}
-		// 根据出入库方向执行不同的操作
-		if inventoryType.Type == 1 {
-			if notExist {
-				// 不存在就插入记录新增映射
-				db.Model(model.Stock{}).Create(&newStock)
-			} else {
-				// 存在就更新记录
-				db.Model(model.Stock{}).Where("warehouse=? AND goods=?", Warehouse, goodsOrder.Goods).Update("quantity", stock.Quantity+goodsOrder.Amount)
-			}
-		} else {
-			if notExist {
-				// 无仓库货品映射，无法执行出库操作
-				context.JSON(http.StatusBadRequest, gin.H{
-					"message": "The goods is not included in the warehouse and cannot be outbound",
-					"code":    405,
-					"detail":  "",
-				})
-				return 1
-			} else {
-				// 存在就更新记录
-				if stock.Quantity < goodsOrder.Amount {
-					context.JSON(http.StatusBadRequest, gin.H{
-						"message": "Including outbound goods with insufficient stock",
-						"code":    406,
-						"detail":  "",
-					})
-					return 1
-				} else {
-					var updateData = map[string]interface{}{
-						"quantity": stock.Quantity - goodsOrder.Amount,
-					}
-					db.Model(model.Stock{}).Where("warehouse=? AND goods=?", Warehouse, goodsOrder.Goods).Updates(updateData)
-				}
-			}
-		}
-	}
-	return 0
 }
