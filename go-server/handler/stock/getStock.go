da578e6c7a3cd73a0c94050dd83f2b0a13562e6a
LanceHE6 <2765543491@qq.com>
2024-06-07 17:56:38 +0800

    feat.参数缺失判断 #docker-push

--- go-server/handler/stock/getStock.go
+++ go-server/handler/stock/getStock.go
@@ -9,6 +9,13 @@ func GetStockRequest(context *gin.Context) {
 	// 获取库存信息
 	goods := context.Query("goods")
 	warehouse := context.Query("warehouse")
+	if goods == "" || warehouse == "" {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Missing parameters",
+			"code":    400,
+		})
+		return
+	}
 	quantity := GetStock(warehouse, goods)
 
 	context.JSON(http.StatusOK, gin.H{
