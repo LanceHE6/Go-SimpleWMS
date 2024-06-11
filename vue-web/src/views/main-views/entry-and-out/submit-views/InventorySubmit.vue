e4df9d885c7df744b2a3c874626c876a9bdfa65f
CM-Silence <757902252@qq.com>
2024-06-11 15:29:02 +0800

    [refactor]文件位置改动

---
+++ vue-web/src/views/main-views/entry-and-out/submit-views/InventorySubmit.vue
@@ -0,0 +1,54 @@
+<template>
+  <submit-view>
+
+  </submit-view>
+</template>
+
+<script setup>
+import SubmitView from "@/components/SubmitView.vue";
+import {reactive} from "vue";
+
+/**
+ * 添加数据时所用到的对象
+ * */
+const form = reactive({
+  data : {
+    date: '',
+    number: '',
+    department: '',
+    inventory_type: '',
+    goods_list: [],
+    warehouse: '',
+    manufacturer: '',
+    operator: '',
+    comment: '',
+  },
+  dataType:{
+    date:'String',
+    number: 'String',
+    department: 'String',
+    inventory_type: 'String',
+    goods_list: 'Array',
+    warehouse: 'String',
+    manufacturer: 'String',
+    operator: 'String',
+    comment: 'String',
+  },
+  dataNum: 9,
+  rules: {
+    inventory_type:[
+      { required: 'true', message: '请选择出入库类型', trigger: 'blur' }
+    ],
+    warehouse:[
+      { required: 'true', message: '请选择所属仓库', trigger: 'blur' }
+    ],
+    operator:[
+      { required: 'true', message: '请选择操作员', trigger: 'blur' }
+    ],
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
