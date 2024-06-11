983660153dd8837e7ecde4370922efe18b9e45af
LanceHE6 <2765543491@qq.com>
2024-06-11 09:37:38 +0800

    chaos.版本号迭代

--- go-server/handler/version.go
+++ go-server/handler/version.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-const version = "v0.0.4.20240610_Alpha"
+const version = "v0.0.5.20240611_Alpha"
 
 func GetVersion(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{
