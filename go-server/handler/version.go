60d06f5b5a366df4b884f79626f4e3617c6690d7
LanceHE6 <2765543491@qq.com>
2024-06-13 18:16:06 +0800

    refactor.重构返回数据结构 #docker-push

--- go-server/handler/version.go
+++ go-server/handler/version.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const version = "v0.3.6.20240613_Alpha"
+const version = "v0.3.7.20240613_Alpha"
 
 func GetVersion(context *gin.Context) {
 	context.JSON(http.StatusOK, response.Response(200, "Hello Simple-WMS", gin.H{
