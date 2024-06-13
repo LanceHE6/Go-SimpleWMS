36f8b87fc16d6a660bbd77431d57ac9548d907ad
CM-Silence <757902252@qq.com>
2024-06-13 08:23:16 +0800

    [refactor]合并TableHeader和LargeTableHeader两个子组件

--- vue-web/src/components/LargeTableHeader.vue
+++ vue-web/src/components/LargeTableHeader.vue
@@ -1,76 +1,15 @@
 <template>
-  <el-button v-if="operations.add" type="primary" icon="Plus" @click="add">
-    添加
-  </el-button>
 
-  <el-button v-if="operations.upload" type="primary" icon="Download" @click="upload" plain>
-    导入
-  </el-button>
-
-
-  <el-button v-if="operations.download" type="primary" icon="Upload" @click="download" plain>
-    导出
-  </el-button>
-
-  <el-button v-if="operations.print" type="warning" icon="Printer" @click="print" plain>
-    打印
-  </el-button>
-
-  <el-input
-      v-model="search"
-      style="width: 240px; margin-left: 20px; margin-right: 10px;"
-      placeholder="关键字搜索"
-      clearable
-  />
-  <el-button type="success" icon="Search" @click="searchChange">
-    搜索
-  </el-button>
-
-  <el-button type="primary" icon="Refresh" @click="refresh" plain style="margin-left: 10px">
-    刷新
-  </el-button>
 </template>
 
 <script setup>
-//对外事件列表
-import {ref} from "vue";
-
 const prop = defineProps({
-  operations:{
-    type: Object,
-    default: () => null,
-    description: '表格支持的操作, 包含增删查改以及导入导出等等(通常无需手动配置)'
-  }
+
 })
 
 const emit = defineEmits(["add", "download", "upload", "search", "print", "refresh"]);
 
-//搜索栏文字
-const search = ref('')
 
-function add(){
-  emit("add");
-}
-
-function download(){
-  emit("download");
-}
-
-function upload(){
-  emit("upload");
-}
-
-function print(){
-  emit("print");
-}
-
-function searchChange(){
-  emit("search", search.value)
-}
-
-function refresh(){
-  emit("refresh")
-}
 </script>
 
 <style scoped>
