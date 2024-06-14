39baf878376db3de79a7cc6622196ba973bd45b4
CM-Silence <757902252@qq.com>
2024-06-14 11:21:10 +0800

    [refactor]页面语言默认改为中文

--- vue-web/src/App.vue
+++ vue-web/src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <!--  根据路由展示页面-->
-  <router-view/>
+  <el-config-provider :locale="zhCn">
+    <!--  根据路由展示页面-->
+    <router-view/>
+  </el-config-provider>
 </template>
 
 <script setup>
 import Zoom from '@/utils/Zoom.js'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import {onMounted} from "vue";
 
 onMounted(() => {
