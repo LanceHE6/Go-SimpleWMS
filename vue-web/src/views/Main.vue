c7f1462cb31f9392f31890547fd5c382e930dfa4
CM-Silence <757902252@qq.com>
2024-06-19 10:48:40 +0800

    [doc]版本号更新

--- vue-web/src/views/Main.vue
+++ vue-web/src/views/Main.vue
@@ -144,7 +144,7 @@ function help(){
 async function about(){
   let go_simpleWMS_version = 'v0.0.0_Alpha'
   let go_server_version = 'unknown'
-  let vue_web_version = 'v0.4.1.20240618_Alpha'
+  let vue_web_version = 'v0.4.3.20240619_Alpha'
   const result = await axiosGet({url: '/ping'})
   if(result){
     go_server_version = result.data.version
