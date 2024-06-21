087457e1dae0eab07f799b24e50e11ab053cdd9c
CM-Silence <757902252@qq.com>
2024-06-21 09:10:30 +0800

    [feat]新增用户中心界面

---
+++ vue-web/src/views/main-views/UserCenter.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container
+    class="main-body"
+  >
+    用户中心
+  </el-container>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.main-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start; /* 子元素在父容器中垂直分布 */
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+}
+</style>
