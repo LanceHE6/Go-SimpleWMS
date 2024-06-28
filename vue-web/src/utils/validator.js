46f9d9e3943005fd6c99648c4d7540b496e7765c
CM-Silence <757902252@qq.com>
2024-06-28 09:43:49 +0800

    [feat]新增约束条件

--- vue-web/src/utils/validator.js
+++ vue-web/src/utils/validator.js
@@ -24,6 +24,31 @@ export const isNotSame = (rule, value, callback, objValue, message='两次输入
     }
 }
 
+//条件内容不为空约束
+export const isNotEmptyCondition = (rule, value, callback, objValue, message='内容不能为空') => {
+    if(objValue === ''){
+        callback()
+    }
+    if(value === '' && objValue !== ''){
+        callback(new Error(message))
+    }
+    else{
+        callback()
+    }
+}
+export const isEmail = (rule, value, callback, message='邮箱格式不正确') => {
+    if(value === ''){
+        callback()
+    }
+    const reg = /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+    if(reg.test(value)){
+        callback()
+    }
+    else{
+        callback(new Error(message))
+    }
+}
+
 //密码合法约束
 export const isPasswordValid = (rule, value, callback) => {
     if(value === '' || (/[a-zA-Z]/.test(value) && /[0-9]/.test(value))){
