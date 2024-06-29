c0e638fcfdb0a596100ccc7e3ff493401e083dab
LanceHE6 <2765543491@qq.com>
2024-06-29 14:19:54 +0800

    chaos.增加函数注释

--- go-server/utils/generate_uuid.go
+++ go-server/utils/generate_uuid.go
@@ -2,6 +2,7 @@ package utils
 
 import "github.com/google/uuid"
 
+// GenerateUuid 生成指定长度的UUID
 func GenerateUuid(length int) string {
 	newUuid := uuid.New().String()
 	return newUuid[len(newUuid)-length:]
